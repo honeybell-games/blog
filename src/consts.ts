92b9bf1d602256151dafbb5bbb16be93abe72158
honeybell-games <stanimirgevel@gmail.com>
2023-10-26 17:38:11 +0200

    steam link added

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,5 @@ export const SITE_TITLE = "Honeybell Games Blog";
 export const SITE_DESCRIPTION =
   "This blog was crafted with love using the template NimbusNarratives | A personal blog by Abdullah Elsayed (@abdllahdev)";
 export const TWITTER_HANDLE = "https://twitter.com/honeybellgames";
-export const WEBSITE_NAME = "https://honeybellgames.com/";
+export const WEBSITE_NAME = "https://honeybellgames.com/"
+export const STEAM_HANDLE = "https://store.steampowered.com/search/?developer=Honeybell%20Games"
